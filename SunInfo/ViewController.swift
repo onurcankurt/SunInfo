5188016930b2ee7ed26dbba9b993625eec83376b
Onurcan Kurt <onurcankurt96@gmail.com>
2024-07-28 21:28:02 +0300

    Initial Commit

---
+++ SunInfo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SunInfo
+//
+//  Created by onur on 28.07.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
